48707ee15f6f8ea2b1a3a5a71302bd2e0c90d5af
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-09-10 17:52:27 +0300

    .

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState('');
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=7353222a869376e12fb1f3204590cae3`;
+  const url = ;
 
   async function searchLocation(e) {
     if (e.key === 'Enter') {
