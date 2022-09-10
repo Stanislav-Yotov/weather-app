251272fb292a6571f7f3d1a6cb5da5a4510b66b3
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-09-10 12:01:28 +0300

    first commit

---
+++ src/components/Weather/WeatherTop.js
@@ -0,0 +1,16 @@
+function WeatherTop(props) {
+    return (
+        <div className='container'>
+            <div className='top'>
+                <div className='location'>
+                    <p>Vratsa</p>
+                </div>
+                <div className='temp'>
+                    <p>23°C</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default WeatherTop;
